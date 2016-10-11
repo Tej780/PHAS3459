d9928de77fde2bae6ae424f3b4d6bda6ad3b6dcd
Tej780 <tej780@hotmail.co.uk>
2016-10-11 18:10:51 +0100

    Made and finished(?) ParticleMain

--- git/java/module2/FallingParticle.java
+++ git/java/module2/FallingParticle.java
@@ -1,11 +1,49 @@
 public class FallingParticle{
 
-	static final double g=-9.81;
-	double m,d,t,z,v;
+	static final double g=9.81;
+	double m,d;
+	double t = 0;
+	double v = 0;
+	double z;
 
-	public FallingParticle(mass,drag){
-		
-}
+	public FallingParticle(double mass, double drag){
+		m=mass;
+		d=drag;
+					 }
+	public void setZ(double Z){
+		z=Z;
+			  }
+	public double getZ(){
+		return z;
+			    }
 
+	public void setV(double V){
+		v=V;
+			  }
+	public double getV(){
+		return v;
+			    }
 
-}
+	public double getT(){
+		return t;
+			    }
+
+	public void doTimeStep(double deltaT){
+		double a = (d*v*v)/m - g;
+		v = v + a*deltaT;
+		z = z + v*deltaT;
+					     }
+
+	public void drop(double deltaT){
+		while (z>0){
+			this.doTimeStep(deltaT);
+			t+=deltaT;
+			   }
+		System.out.println("Time Taken to fall: " + t + "seconds");
+		System.out.println("Velocity at the bottom: " + v + "" );
+		t=0;
+		v=0;
+				       }
+
+			    }
+
---
+++ git/java/module2/ParticleMain.java
@@ -0,0 +1,20 @@
+public class ParticleMain{
+	public static void main(String[] args){
+        	FallingParticle tej = new FallingParticle(6.3,4.1);
+                tej.setZ(8);
+		tej.drop(0.5);
+		tej.setZ(8);
+		tej.drop(0.1);
+		tej.setZ(8);
+		tej.drop(0.01);
+		tej.setZ(8);
+		tej.drop(0.001);
+		tej.setZ(8);
+		tej.drop(0.0001);
+                                              }		
+
+
+
+					      	
+
+			 }
