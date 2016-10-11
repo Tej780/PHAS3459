6cba2a511ae25a4a57e7d7118e0f8a140d5e3f61
Tej780 <tej780@hotmail.co.uk>
2016-10-11 16:31:27 +0100

    Created and completed TestComplex.java

--- git/java/module2/Complex.java
+++ git/java/module2/Complex.java
@@ -4,50 +4,66 @@ public class Complex{
 	public Complex(double a, double b){
 		x=a;
 		y=b;
-}
+		    			  }
 
 	public double real(){
 		return this.x;
-}
+			    }
 
 	public double img(){
 		return this.y;
-}
+			   }
 
 	public double modulus(){
 		double mod = Math.sqrt(this.x*this.x + this.y*this.y);
 		return mod;
-}
+			       }
 
 	public double angle(){
 		double arg = Math.atan(this.y/this.x);
 		return arg;
-}
+			     }
 
 	public Complex conjugate(){
 		double u = this.x;
 		double v = -1*(this.y);
 		Complex w = new Complex(u,v);
 		return w;
-}
+				  }
+
+	public Complex normalised(){                    
+		double xhat = this.x/this.modulus();
+                double yhat = this.y/this.modulus();
+                Complex rhat = new Complex(xhat, yhat);
+                return rhat;
+				   }
 
 	public boolean equals(Complex c){
 		if(this == c){
 			return true;
-}		else{
+			     }		
+		else{
 			return false;
-}
-}
+		    }
+					}
 
 	public String toString(){
 		String z;
 		if (this.y<0){
 			z = (this.x + " - " + -1*this.y + "i");
-}		else {
+			     }		
+		else {
 			z = (this.x + " + " + this.y + "i");
-}
+		     }
 		return z;
-}
+				}
+
+	public static Complex setFromModulusAngle(double mag, double ang){
+		double x = mag*Math.cos(ang);
+		double y = mag*Math.sin(ang);
+		Complex z = new Complex(x,y);
+		return z;
+									 }
 
 	static Complex ONE = new Complex(1,0);
 	static Complex ZERO = new Complex();
@@ -65,48 +81,36 @@ public class Complex{
 		System.out.println(z.real());
 		System.out.println(z.img());
 
-}
-
-	public static Complex setFromModulusAngle(double mag, double ang){
-		double x = mag*Math.cos(ang);
-		double y = mag*Math.sin(ang);
-		Complex z = new Complex(x,y);
-		return z;
-}
+					      }
 
 	public static Complex add(Complex z, Complex w){
 		double a = z.x + w.x;
 		double b = z.y + w.y;
 		Complex c = new Complex(a,b);
 		return c;
-}
+						       }
 
 	public static Complex subtract(Complex z, Complex w){
                 double a = z.x - w.x;
                 double b = z.y - w.y;
                 Complex c = new Complex(a,b);
                 return c;
-}
+							    }
 
 	public static Complex multiply(Complex z, Complex w){
                 double a = z.x*w.x - z.y*w.y;
                 double b = z.y*w.x + w.y*z.x;
                 Complex c = new Complex(a,b);
                 return c;
-}
+							    }
 
 	  public static Complex divide(Complex z, Complex w){
                 Complex a = Complex.multiply(z,w.conjugate());
                 double b = w.x*w.x + w.y*w.y;
                 Complex c = new Complex(a.x/b, a.y/b);
                 return c;
-}
+							    }
 
-	public Complex normalised(){
-		double xhat = this.x/this.modulus();
-                double yhat = this.y/this.modulus();
-                Complex rhat = new Complex(xhat, yhat);
-                return rhat;
-}
+      
 
 }
