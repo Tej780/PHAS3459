5c77758a828d4ead19d26d23ad7cc4806194a05e
Tej780 <tej780@hotmail.co.uk>
2016-10-16 01:43:05 +0100

    Finished and uploaded all of Module 2, which means I am also finished with module 3

--- src/module3/FallingParticle.java
+++ src/module3/FallingParticle.java
@@ -1,17 +1,21 @@
-//package module2;
+package module2;
+
 public class FallingParticle{
-	//acceleration due to gravity is constant and has the same value for all
-	//FallingParticle objects
+	
+	//acceleration due to gravity is constant and has the same value for all FallingParticle objects
 	static final double g=9.81;
 	double m,d;
-	//I have chosen to assign an initial value of 0 to t and v as in most cases the Particle will begin moving from rest at time zero. If the user wishes to use a different value of either t or v, they may use the set() functions.
+	//I have chosen to assign an initial value of 0 to t and v as in most cases the Particle will begin moving from rest at time zero.
+	// If the user wishes to use a different value of either t or v, they may use the set() functions.
 	double t = 0;
 	double v = 0;
 	double z;
+	
 	//I have omited an empty constructor as I want to avoid an issue with 0 mass
+
 	public FallingParticle(double mass, double drag) throws IllegalArgumentException{
 		try{
-			if(mass <=0){
+			if(mass <=0){//avoids unphysical systems with negative masses
 				throw new IllegalArgumentException();
 				}
 			m=mass;
@@ -20,9 +24,15 @@ public class FallingParticle{
 			System.out.println("Mass must be positive!");
 						}
 					 }
+	
+
+	/**
+	*Sets the initial height of the Particle
+	*@param Z initial height
+	*/
 	public void setZ(double Z) throws IllegalArgumentException{
 		try{
-			if(Z<=0){
+			if(Z<=0){//prevents the particle from starting 'beneath the ground'
 				throw new IllegalArgumentException();
 				}
 			z=Z;
@@ -32,22 +42,39 @@ public class FallingParticle{
 						}
 			
 			  }
+
+	/**
+	*Returns the current position of the Particle
+	*@return position
+	*/
 	public double getZ(){
 		return z;
 			    }
 
+	/**
+	*Sets the velocity of the Particle
+	*@param V velocity
+	*/
 	public void setV(double V){
 		v=V;
 			  }
+	/**
+	*Returns the current velocity of the Particle
+	*@return velocity
+	*/
 	public double getV(){
 		return v;
 			    }
 
+	/**
+	*Returns the current time
+	*/
 	public double getT(){
 		return t;
 			    }
 
-	/**Recalculates the new acceleration, position and velocity of the particle after each step dt
+	/**
+	*Recalculates the new acceleration, position and velocity of the particle after a time step dt
 	*@param deltaT
 	*
 	*/
@@ -56,7 +83,9 @@ public class FallingParticle{
 		v = v + a*deltaT;
 		z = z + v*deltaT;
 					     }
-	/**Simulates the motion of the particle once it is dropped. Runs while the particle is 'above the ground' and calls doTimeStep to recalculate and update the position, velocity and acceleration of the particle. Prints out the final velocity and time.
+	/**
+	*Simulates the motion of the particle once it is dropped.
+	*Runs while the particle is 'above the ground' and calls doTimeStep to recalculate and update the position, velocity and acceleration of the particle. Prints out the final velocity and time.
 	*@param deltaT
 	*/
 	public void drop(double deltaT){
@@ -64,15 +93,21 @@ public class FallingParticle{
 			this.doTimeStep(deltaT);
 			t+=deltaT;
 			   }
-		System.out.println("Time Taken to fall: " + t + "seconds");
+		System.out.println("Time interval dt: " + deltaT + "s" );
+		System.out.println("Time Taken to fall: " + t + "s");
 		System.out.println("Velocity at the bottom: " + v + "m/s" );
+		//resets the value of t and v so the same object can be re-dropped
+		v=0;
 		t=0;
 				       }
+
 	// simply prints out the mass and drag of the particular Particle object
 	public String toString(){
 	return "An object of mass "+this.m+"kg falling in a drag coefficient of "+this.d+"kg/m";
 				}
-	public static void main(String[] args){
+
+/*	
+		public static void main(String[] args){
 		FallingParticle n = new FallingParticle(5,5);
 		n.setZ(8);
 		n.drop(0.01);
@@ -80,5 +115,6 @@ public class FallingParticle{
 		FallingParticle g = new FallingParticle(-5,5);
 		System.out.println(g.d);
 						}
+*/
 			    }
 
--- src/module3/ParticleMain.java
+++ src/module3/ParticleMain.java
@@ -1,30 +1,21 @@
-//package module2;
+package module2;
 
 public class ParticleMain{
 	public static void main(String[] args){
         	FallingParticle tej = new FallingParticle(6.3,4.1);
 		System.out.println(tej);
-		//Each time the particle is 'dropped', the position and velocity must be reset before the object can be dropped again.
+		//Each time the particle is 'dropped', the position must be reset before the object can be dropped again.
                 tej.setZ(8);
-		tej.setV(0);
 		tej.drop(0.5);
 		tej.setZ(8);
-		tej.setV(0);
 		tej.drop(0.1);
 		tej.setZ(8);
-		tej.setV(0);
 		tej.drop(0.01);
 		tej.setZ(8);
-		tej.setV(0);
 		tej.drop(0.001);
 		tej.setZ(8);
-		tej.setV(0);
 		tej.drop(0.0001);
 		System.out.println("A smaller dt leads to a higher resolution, as such the time taken and final velocity tend towards particular values in the limit dt -> 0");
-                                              }		
-
-
-
-					      	
-
+                System.out.println("\nSomething else I noticed is that, if dt is too great(i.e. the resolution is too low) then the velocity can actually become 'too negative' and the object essentially ends up 'faster than terminal velocity'. When this happens, the acceleration actually becomes positive in an attempt to compensate. This explains why dt = 0.5 results in such a wildly inaccurate number, the resolution is far too low. I don't expect this observation to warrent any extra marks, and I doubt anyone else cares much, but I found it nifty :D");         
+		                  }
 			 }
