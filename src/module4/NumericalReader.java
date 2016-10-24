34c970b5cb5e843436ed45a6f8d698c9e704f8f3
Tej780 <tej780@hotmail.co.uk>
2016-10-24 02:09:46 +0100

    Continuing with NumericalReader

---
+++ src/module4/NumericalReader.java
@@ -0,0 +1,84 @@
+//package module4;
+import java.util.Scanner;
+import java.io.*;
+import java.net.*;
+public class NumericalReader{
+	
+	public NumericalReader(){}
+
+	private static double minValue, maxValue,sumOfValues;
+	private static int nValues;
+
+	public static String getStringFromKeyboard(){
+		Scanner s = new Scanner (System.in);
+		System.out.println("Enter the location to store the datafile:");
+		String input = s.next();
+		return input;
+						}
+
+	public BufferedReader brFromURL(String url){
+        BufferedReader br = null;
+        try{
+                URL u = new URL(url);
+                InputStream is = u.openStream();
+                InputStreamReader isr = new InputStreamReader(is);
+                br = new BufferedReader(isr);
+
+        }catch(FileNotFoundException e){
+                System.out.println(e);
+        }catch(MalformedURLException e){
+                System.out.println(e);
+        }catch(IOException e){
+                System.out.println(e);
+        }
+        return br;
+                               }
+
+	public static void analysisStart(String dataFile){
+		minValue=0;
+		maxValue=0;
+		nValues=0;
+		sumOfValues=0;
+		try{
+		FileWriter f = new FileWriter(dataFile);
+		BufferedWriter bw = new BufferedWriter(f);
+		PrintWriter pw = new PrintWriter(bw);
+		}catch(IOException e){
+			System.out.println(e);
+					}
+				}
+
+	public void analyseData(String line){
+		if(line.trim().isEmpty() || line.startsWith("x")){
+		}else {
+			System.out.println(line);
+			nValues++;
+		}			
+				}
+			
+
+	public void analysisEnd(){
+		System.out.println("Minimum value: "+minValue);
+		System.out.println("Maximum value: "+maxValue);
+		System.out.println("Number of datapoints: "+nValues);
+		System.out.println("Sum of values: "+sumOfValues);
+	}
+
+		public static void main(String[] args){
+		String datafile = "N:"+ File.separator+"mywork"+File.separator+"number.txt";
+		String location = NumericalReader.getStringFromKeyboard();
+		String savefile = location + File.separator + datafile;
+		NumericalReader nr = new NumericalReader();
+		BufferedReader br = nr.brFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_data2.txt");
+		String line = "";
+		nr.analysisStart(savefile);
+		try{
+		while((line = br.readLine()) != null){
+			
+			nr.analyseData(line);
+						}
+		}catch(IOException e){
+			System.out.println(e);
+		}
+			}
+}
---
+++ src/module4/WordCounter.java
@@ -0,0 +1,74 @@
+//package module4;
+import java.util.Scanner;
+import java.io.*;
+import java.net.*;
+public class WordCounter{
+	public static BufferedReader brFromURL(String urlName){
+	BufferedReader br = null;
+	try{
+		URL u = new URL(urlName);
+		InputStream is = u.openStream();
+		InputStreamReader isr = new InputStreamReader(is);
+		br = new BufferedReader(isr);		
+		
+	}catch(FileNotFoundException e){
+		System.out.println(e);
+	}catch(MalformedURLException e){
+		System.out.println(e);
+	}catch(IOException e){
+		System.out.println(e);
+	}
+	return br;
+	                       }
+
+	public static BufferedReader brFromFile(String fileName){
+		BufferedReader br = null;
+		try{
+
+		FileReader fr = new FileReader(fileName);
+		br = new BufferedReader(fr);
+		}catch(FileNotFoundException e){
+			System.out.println(e);
+		}
+		return br;
+}
+
+	public static int countWordsInResource(BufferedReader dataAsBR){
+		int counter = 0;
+		try(
+			Scanner s = new Scanner(dataAsBR);
+		){
+		while(s.hasNext()){
+			String token = s.next();
+			counter++;
+				}
+		}
+		return counter;
+}
+
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		System.out.println("Are you using a (f)ile or (u)rl?");
+		BufferedReader br=null;
+		int x=0;
+		do{
+			String type = s.next();
+		if (type.charAt(0)=='u'){
+			System.out.println("Please enter the URL");
+			String URL = s.next();
+			br = brFromURL(URL);
+			x++;
+		}else if(type.charAt(0)=='f'){
+			System.out.println("Please enter the File Name");
+			String file = s.next();
+			br = brFromURL(file);
+			x++;
+		}else{
+			System.out.println("Unnacceptable input, please try again");
+		}}while(x==0);
+		System.out.println("The total number of words in this resource: " +countWordsInResource(br));
+
+		}
+
+
+}
