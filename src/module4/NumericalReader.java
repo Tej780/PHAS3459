853d41767e6d09ca3f3c8caad6eaa92185baaf7a
tej780 <tej780@hotmail.co.uk>
2016-10-30 00:39:47 +0100

    Finished commenting on both WordCounter and NumericalReader

--- src/module4/NumericalReader.java
+++ src/module4/NumericalReader.java
@@ -3,16 +3,21 @@ import java.util.Scanner;
 import java.lang.Double;
 import java.io.*;
 import java.net.*;
+
 public class NumericalReader{
 
 	public NumericalReader(){}
 
+	//declare member variables
 	private static double minValue, maxValue,sumOfValues;
 	private static int nValues;
 	FileWriter fw = null;
 	BufferedWriter bw = null;	
 	PrintWriter pw = null;
 	
+	/**Allows the user to specify the directory in which to store the data files
+	 * @return input, the string entered by the user to represent to directory
+	 */
 	public static String getStringFromKeyboard(){
 		Scanner s = new Scanner (System.in);
 		System.out.println("Enter the location to store the datafile:");
@@ -21,6 +26,10 @@ public class NumericalReader{
 		return input;
 	}
 
+	/**Stores the character file found at the given URL within the BufferedReader
+	 * @param url, a string representing the URL that points to the data file
+	 * @return br, the BufferedReader object holding the data stored at the given URL
+	 */
 	public BufferedReader brFromURL(String url){
 		BufferedReader br = null;
 		try{
@@ -39,6 +48,9 @@ public class NumericalReader{
 		return br;
 	}
 
+	/**Initialize the variables for analyzing the data, and creates the file to store the analyzed data
+	 * @param dataFile, string containing the location at which the file will be stored
+	 */
 	public void analysisStart(String dataFile){
 		minValue=10^99;
 		maxValue=0;
@@ -51,30 +63,33 @@ public class NumericalReader{
 		}catch(IOException e){
 			System.out.println(e);
 		}
-
 	}
 
+	/**Finds each individual number, prints it to both the console and data file. 
+	 * Finds the max, min, average and total number of data points
+	 * @param line, takes each line of the data file one by one for analysis
+	 */
 	public void analyseData(String line){
 		if(line.trim().isEmpty() || Character.isLetter(line.charAt(0))){
+			//do nothing
 		}else {
-
-			String[] parts = line.split("\t|     | ");
-			for (int i=0;i<parts.length;i++){
+			Scanner s = new Scanner(line);
+			while(s.hasNext()){
+				String data = s.next();
 				bw = new BufferedWriter(fw);
 				pw = new PrintWriter(bw);
-				pw.println(parts[i]);
+				//print data to data file
+				pw.println(data);
 				pw.flush();
-				double value = Double.parseDouble(parts[i]);
+				//turn data string into double for use in calculations
+				double value = Double.parseDouble(data);
 				System.out.println(value);
 				sumOfValues += value;
 				nValues++;
 				if(value<minValue){
-
 					minValue = value;
 				}else if(value>maxValue){
-
 					maxValue = value;
-
 				}
 				
 			}
@@ -82,6 +97,9 @@ public class NumericalReader{
 	}
 
 
+	/**Print out results of data analysis and close all write streams
+	 * 
+	 */
 	public void analysisEnd(){
 		System.out.println("Minimum value: "+minValue);
 		System.out.println("Maximum value: "+maxValue);
@@ -99,11 +117,14 @@ public class NumericalReader{
 	}
 
 	public static void main(String[] args){
+		//Choose where to store the two data files
 		String dataFile1 = "numbers1.txt";
 		String dataFile2 = "numbers2.txt";
 		String location = NumericalReader.getStringFromKeyboard();
 		String saveFile1 = location + File.separator + dataFile1;
 		String saveFile2 = location + File.separator + dataFile2;
+		
+		//Analyze data from first URL
 		NumericalReader nr1 = new NumericalReader();
 		BufferedReader br1 = nr1.brFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_data1.txt");
 		String line = "";
@@ -118,6 +139,7 @@ public class NumericalReader{
 		}
 		nr1.analysisEnd();
 
+		//Analyse data from second URL
 		NumericalReader nr2 = new NumericalReader();
 		BufferedReader br2 = nr2.brFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_data2.txt");
 		line = "";
@@ -132,7 +154,5 @@ public class NumericalReader{
 			System.out.println(e);
 		}
 		nr2.analysisEnd();
-
-
 	}
 }
--- src/module4/WordCounter.java
+++ src/module4/WordCounter.java
@@ -3,47 +3,68 @@ import java.util.Scanner;
 import java.io.*;
 import java.net.*;
 public class WordCounter{
-	public static BufferedReader brFromURL(String urlName){
-	BufferedReader br = null;
-	try{
-		URL u = new URL(urlName);
-		InputStream is = u.openStream();
-		InputStreamReader isr = new InputStreamReader(is);
-		br = new BufferedReader(isr);		
-		
-	}catch(FileNotFoundException e){
-		System.out.println(e);
-	}catch(MalformedURLException e){
-		System.out.println(e);
-	}catch(IOException e){
-		System.out.println(e);
-	}
-	return br;
-	                       }
 
+	/**Creates a BufferedReader object based on a file found at a given URL
+	 * @param urlName
+	 * @return
+	 */
+	public static BufferedReader brFromURL(String urlName){
+		BufferedReader br = null;
+		try{
+			//creates an abstract URL object based off the input string
+			URL u = new URL(urlName);
+			//opens an input stream connection to the URL
+			InputStream is = u.openStream();
+			//reads the bytes from the input stream and decodes them as characters (bytestream -> characterstream)
+			InputStreamReader isr = new InputStreamReader(is);
+			//reads characters from character stream
+			br = new BufferedReader(isr);		
+
+		}catch(FileNotFoundException e){
+			System.out.println(e);
+		}catch(MalformedURLException e){
+			System.out.println(e);
+		}catch(IOException e){
+			System.out.println(e);
+		}
+		return br;
+	}
+
+	/**Creates a BufferedReader object based on a file found at a given file location
+	 * @param fileName
+	 * @return
+	 */
 	public static BufferedReader brFromFile(String fileName){
 		BufferedReader br = null;
 		try{
-		FileReader fr = new FileReader(fileName);
-		br = new BufferedReader(fr);
+			//abstract representation of the file stored at the given directory string
+			File f = new File(fileName);
+			//enables reading the character file
+			FileReader fr = new FileReader(f);
+			//stores files within a BufferedReader object
+			br = new BufferedReader(fr);
 		}catch(FileNotFoundException e){
 			System.out.println(e);
 		}
 		return br;
-}
+	}
 
+	/**Returns the number of words (strings of characters separated by whitespace) stored within the BufferedReader
+	 * @param dataAsBR
+	 * @return
+	 */
 	public static int countWordsInResource(BufferedReader dataAsBR){
 		int counter = 0;
 		try(
-			Scanner s = new Scanner(dataAsBR);
-		){
-		while(s.hasNext()){
-			s.next();
-			counter++;
-				}
+				Scanner s = new Scanner(dataAsBR);
+				){
+			while(s.hasNext()){
+				s.next();
+				counter++;
+			}
 		}
 		return counter;
-}
+	}
 
 	public static void main(String[] args){
 		Scanner s = new Scanner(System.in);
@@ -52,23 +73,23 @@ public class WordCounter{
 		int x=0;
 		do{
 			String type = s.next();
-		if (type.charAt(0)=='u'){
-			System.out.println("Please enter the URL");
-			String URL = s.next();
-			br = brFromURL(URL);
-			x++;
-		}else if(type.charAt(0)=='f'){
-			System.out.println("Please enter the File Name");
-			String file = s.next();
-			br = brFromURL(file);
-			x++;
-			
-		}else{
-			System.out.println("Unnacceptable input, please try again");
-		}}while(x==0);
+			if (type.charAt(0)=='u'){
+				System.out.println("Please enter the URL");
+				String URL = s.next();
+				br = brFromURL(URL);
+				x++;
+			}else if(type.charAt(0)=='f'){
+				System.out.println("Please enter the File Name");
+				String file = s.next();
+				br = brFromFile(file);
+				x++;
+
+			}else{
+				System.out.println("Unnacceptable input, please try again");
+			}}while(x==0);
 		System.out.println("The total number of words in this resource: " +countWordsInResource(br));
 		s.close();
-		}
+	}
 
 
 }
