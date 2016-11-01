f587a590c0796a7482691bd783b067c5a8640a92
Tej780 <tej780@hotmail.co.uk>
2016-11-01 04:03:00 +0000

    Deleted git on my laptop and redid it all, yaaaaay (sarcasm)

--- src/module5/SquareMatrix.java
+++ src/module5/SquareMatrix.java
@@ -1,3 +1,74 @@
+<<<<<<< HEAD
+//package module5;
+
+public class SquareMatrix{
+	
+	public SquareMatrix[][](double[][] elements) throws IllegalArgumentException{
+		if(elements.length != elements[0].length){
+			throw new IllegalArgumentException();
+		}
+				}
+
+	public static SquareMatrix empty(SquareMatrix sm){
+		for(int i=0;i<sm.length;i++){
+			for(int j=0;j<sm[0].length;j++){
+				this[i][j]=0;
+						}
+			}
+					}
+
+	public String toString(){
+		String matrix = "";
+		for(int i=0;i<this.length;i++){
+			for(int j=0;j<this.length;j++){
+				matrix = matrix + this[i][j]+"\t";
+			}
+			matrix = matrix + "\n";
+			}
+					}
+
+
+
+
+	public static SquareMatrix add(SquareMatrix sm1, SquareMatrix sm2){
+		if(sm1.length == sm2.length && sm1[0].length==sm2[0].length){
+			SquareMatrix sm = sm1.empty;
+			for (int i=0;i<elements.length;i++){					
+				for(int j=0;j<elements.length;j++){
+					sm[i][j]=sm1[i][j]+sm2[i][j];	
+									}
+								}
+					}
+									}
+	public static SquareMatrix subtract(SquareMatrix sm1, SquareMatrix sm2){
+		if(sm1.length == sm2.length && sm1[0].length==sm2[0].length){
+                        SquareMatrix sm = sm1.empty;
+                        for (int i=0;i<elements.length;i++){
+                        	for(int j=0;j<elements.length;j++){
+                                        sm[i][j]=sm1[i][j]-sm2[i][j];
+
+								}
+							}
+									}
+										}
+	public static SquareMatrix multiply(SquareMatrix sm1, SquareMatrix sm2){
+		if(sm1.length == sm2.length && sm1[0].length==sm2[0].length){
+                        SquareMatrix sm = sm1.empty;
+                        for (int i=0;i<elements.length;i++){                        
+                                for(int j=0;j<elements.length;j++){
+                                        sm[i][j]=sm1[i][j]+sm2[i][j];
+		
+
+
+					}
+							}
+									}
+										}
+}
+
+
+
+=======
 package module5;
 
 public class SquareMatrix {
@@ -19,3 +90,4 @@ public class SquareMatrix {
 		
 	}
 }
+>>>>>>> ba98154b160ff63a8816cd5537ca638d9b19b62e
