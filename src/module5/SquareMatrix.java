9df127c408b2c039ccf8f2ba42f2eefcb068db59
tej780 <tej780@hotmail.co.uk>
2016-11-02 00:20:07 +0000

    Completed SquareMatrix.java, may go back and change methods to non-static

--- src/module5/SquareMatrix.java
+++ src/module5/SquareMatrix.java
@@ -2,67 +2,97 @@
 package module5;
 
 public class SquareMatrix{
-	
-	public SquareMatrix[][](double[][] elements) throws IllegalArgumentException{
+
+	int length;
+	double[][] el;
+
+	public SquareMatrix(double[][] elements) throws IllegalArgumentException{
 		if(elements.length != elements[0].length){
 			throw new IllegalArgumentException();
 		}
-				}
-
-	public static SquareMatrix empty(SquareMatrix sm){
-		for(int i=0;i<sm.length;i++){
-			for(int j=0;j<sm[0].length;j++){
-				this[i][j]=0;
-						}
+		length = elements.length;
+		for(int i=0;i<length;i++){
+			for(int j=0;j<length;j++){
+				el[i][j] = elements [i][j];
 			}
-					}
+		}
+	}
+
+	public SquareMatrix(int i) throws IllegalArgumentException{
+		this.length= i;
+		el = new double[i][i];	
+	}
+
 
 	public String toString(){
 		String matrix = "";
 		for(int i=0;i<this.length;i++){
 			for(int j=0;j<this.length;j++){
-				matrix = matrix + this[i][j]+"\t";
+				matrix = matrix + el[i][j]+"\t";
 			}
 			matrix = matrix + "\n";
+		}
+		return matrix;
+	}
+
+
+
+
+	public static SquareMatrix add(SquareMatrix sm1, SquareMatrix sm2) throws IllegalArgumentException{
+		SquareMatrix sm = new SquareMatrix(sm1.length);
+		if(sm1.length == sm2.length ){	
+			for (int i=0;i<sm1.length;i++){					
+				for(int j=0;j<sm1.length;j++){
+					sm.el[i][j] = sm1.el[i][j] + sm2.el[i][j];	
+				}
 			}
+		}else{
+			throw new IllegalArgumentException();
+
+		}
+		return sm;
+	}
+
+	public static SquareMatrix subtract(SquareMatrix sm1, SquareMatrix sm2) throws IllegalArgumentException{
+		SquareMatrix sm = new SquareMatrix(sm1.length);
+		if(sm1.length == sm2.length ){	
+			for (int i=0;i<sm1.length;i++){					
+				for(int j=0;j<sm1.length;j++){
+					sm.el[i][j] = sm1.el[i][j] - sm2.el[i][j];	
+				}
+			}
+		}else{
+			throw new IllegalArgumentException();
+
+		}
+		return sm;
+	}
+
+	public static SquareMatrix multiply(SquareMatrix sm1, SquareMatrix sm2) throws IllegalArgumentException{
+		SquareMatrix sm = new SquareMatrix(sm1.length);
+		if(sm1.length == sm2.length ){	
+			for (int i=0;i<sm1.length;i++){					
+				for(int j=0;j<sm1.length;j++){
+					for(int k=0;k<sm1.length;k++){
+						sm.el[i][j] += sm1.el[i][k] * sm2.el[k][j];
 					}
+				}
+			}
+		}else{
+			throw new IllegalArgumentException();
 
-
-
-
-	public static SquareMatrix add(SquareMatrix sm1, SquareMatrix sm2){
-		if(sm1.length == sm2.length && sm1[0].length==sm2[0].length){
-			SquareMatrix sm = sm1.empty;
-			for (int i=0;i<elements.length;i++){					
-				for(int j=0;j<elements.length;j++){
-					sm[i][j]=sm1[i][j]+sm2[i][j];	
-									}
-								}
-					}
-									}
-	public static SquareMatrix subtract(SquareMatrix sm1, SquareMatrix sm2){
-		if(sm1.length == sm2.length && sm1[0].length==sm2[0].length){
-                        SquareMatrix sm = sm1.empty;
-                        for (int i=0;i<elements.length;i++){
-                        	for(int j=0;j<elements.length;j++){
-                                        sm[i][j]=sm1[i][j]-sm2[i][j];
-
-								}
-							}
-									}
-										}
-	public static SquareMatrix multiply(SquareMatrix sm1, SquareMatrix sm2){
-		if(sm1.length == sm2.length && sm1[0].length==sm2[0].length){
-                        SquareMatrix sm = sm1.empty;
-                        for (int i=0;i<elements.length;i++){                        
-                                for(int j=0;j<elements.length;j++){
-                                        sm[i][j]=sm1[i][j]+sm2[i][j];
-		
-
-
-					}
-							}
-									}
-										}
+		}
+		return sm;
+	}
+	
+	public static boolean equals(SquareMatrix A,SquareMatrix B) {
+        if (A.length != B.length) throw new RuntimeException("Matricies have different dimensions.");
+        for (int i = 0; i < A.length; i++)
+            for (int j = 0; j < A.length; j++)
+                if (A.el[i][j] != B.el[i][j]) return false;
+        return true;
+    }
+	
+	
 }
 
--- src/module5/TestSquareMatrix.java
+++ src/module5/TestSquareMatrix.java
@@ -3,8 +3,28 @@ package module5;
 public class TestSquareMatrix {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SquareMatrix s1 = new SquareMatrix(2);
+		for(int i=0;i<s1.length;i++){
+			for(int j=0;j<s1.length;j++){
+				s1.el[i][j] = 2*(i+1)*(j+1);
+			}
+		}
+		SquareMatrix s2 = new SquareMatrix(2);
+		for(int i=0;i<s2.length;i++){
+			for(int j=0;j<s2.length;j++){
+				s2.el[i][j] =3*(i+1)*(j+1);
+			}
+		}
+		
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(SquareMatrix.subtract(s1, s2));
+		System.out.println(SquareMatrix.multiply(s1, s2));
+		System.out.println(SquareMatrix.equals(s1,s2));
+		
+		
+		
+		
 	}
 
 }
