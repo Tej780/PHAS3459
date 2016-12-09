f3e290796089bcb3a15d51d74035f6d42f4bf3aa
tej780 <tej780@hotmail.co.uk>
2016-12-09 09:55:36 +0000

    Created MonteCarlo classes

---
+++ src/module8/ThreadsTimer.java
@@ -0,0 +1,14 @@
+package module8;
+
+public class ThreadsTimer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static void singleThread(){
+		
+	}
+
+}
